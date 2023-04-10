96a6bdbe48eab904950f48ff4a925ef10deb0657
Diwan <diwanmuhamad37@gmail.com>
2023-04-10 16:41:17 +0700

    Navbar update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,22 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+
+import logo from '../../images/logo.png'
+
+const NavbarItem = () => {
+    
+}
+
 const Navbar = () => {
     return (
-        <h1>Navbar</h1>
+        <nav className="w-full flex md:justify-between items-centter p-4">
+            <div className="md:flex-[0.5] flex-initial justify-center items-center">
+                <img src={logo} alt="logo" className="w-32 cursor-pointer"/>
+            </div>
+            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+
+            </ul>
+        </nav>
     )
 }
 
