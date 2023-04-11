697b591b4c96a5a6775e6a8b1700a59b896fea8b
Diwan <diwanmuhamad37@gmail.com>
2023-04-11 09:49:21 +0700

    finish navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,11 +17,11 @@ const NavbarItem = ({ title, classProps}) => {
 const Navbar = () => {
     const [toggleMenu, setToggleMenu] = useState(false)
     return (
-        <nav className="w-full flex md:justify-between items-centter p-4">
+        <nav className="w-full flex md:justify-center justify-between items-centter p-4">
             <div className="md:flex-[0.5] flex-initial justify-center items-center">
                 <img src={logo} alt="logo" className="w-32 cursor-pointer"/>
             </div>
-            <ul className="text-white md:flex list-none flex-row justify-between items-center flex-initial">
+            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
                {
                 data.map((item, index) => (                    
                     <NavbarItem key={item+index} title={item}/>
@@ -36,6 +36,22 @@ const Navbar = () => {
                 toggleMenu? <AiOutlineClose fontSize={28} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu(false)}/>
                 :
                 <HiMenuAlt4 fontSize={28} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu(true)}/>
+               }
+               {
+                toggleMenu && (
+                    <ul className='z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+                        flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in
+                    '>
+                        <li className='text-xl w-full my-2'>
+                            <AiOutlineClose onClick={() => setToggleMenu(false)}/>
+                        </li>
+                        {
+                            data.map((item, index) => (                    
+                                <NavbarItem key={item+index} title={item} classProps="my-2 text-lg"/>
+                            ))
+                        }
+                    </ul>
+                )
                }
             </div>
         </nav>
